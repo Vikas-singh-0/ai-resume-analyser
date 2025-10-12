5c468d150add88664fd0d3924f72bd4de77a0cb3
Vikas Singh <s.vikas892354@gmail.com>
2025-10-12 17:13:25 +0530

    get real resumes instead of dummy

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,26 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router'
-import ScoreCircle from './ScoreCircle'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/puter/puter";
+import type { Resume } from "~/types";
 
-function ResumeCard({resume}: {resume: Resume}) {
+function ResumeCard({ resume }: { resume: Resume }) {
+  const { fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(resume.imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+    loadResume();
+  }, [resume.imagePath]);
   return (
-    <Link to={`/resume/${resume.id}`} className='resume-card animate-in fade-in duration-1000'>
-       <div className='resume-card-header'>
-         <div className='flex flex-col gap-2'>
-            <h2 className='!text-black font-bold break-words'>{resume.companyName}</h2>
-            <h3 className='text-lg break-words text-gray-500'>{resume.jobTitle}</h3>
+    <Link
+      to={`/resume/${resume.id}`}
+      className="resume-card animate-in fade-in duration-1000"
+    >
+      <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+          <h2 className="!text-black font-bold break-words">
+            {resume.companyName}
+          </h2>
+          <h3 className="text-lg break-words text-gray-500">
+            {resume.jobTitle}
+          </h3>
         </div>
-        <div className='flex-shrink-0'>
-                <ScoreCircle score={resume.feedback.overallScore}/>
-            </div>
-       </div>
-       <div className='gradiant-border animate-in fade-in duration-1000'>
-        <div className='w-full h-full'>
-            <img src={resume.imagePath} className='w-full h-[350px] max-sm:h-[200px] object-top object-cover' alt='resume'/>
+        <div className="flex-shrink-0">
+          <ScoreCircle score={resume.feedback.overallScore} />
         </div>
-       </div>
+      </div>
+      {resumeUrl && (<div className="gradiant-border animate-in fade-in duration-1000">
+        <div className="w-full h-full">
+          <img
+            src={resumeUrl}
+            className="w-full h-[350px] max-sm:h-[200px] object-top object-cover"
+            alt="resume"
+          />
+        </div>
+      </div>)}
     </Link>
-  )
+  );
 }
 
-export default ResumeCard
+export default ResumeCard;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,8 @@ import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
 import { usePuterStore } from "~/puter/puter";
 import { useLocation, useNavigate } from "react-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import type { Resume } from "~/types";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,93 +15,111 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { isLoading, auth, init } = usePuterStore();
+  const { isLoading, auth, init, kv } = usePuterStore();
   const location = useLocation();
-  const next: string = location.search.split('next=')[1];
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setResumesLoading] = useState(false);
+  const next: string = location.search.split("next=")[1];
   const navigate = useNavigate();
+
   useEffect(() => {
-    if(!auth.isAuthenticated) {
-      navigate('/auth?next=/');
-    } 
+    const loadResumes = async () => {
+      setResumesLoading(true);
+      const resumes = (await kv.list("resume:*", true)) as KVItem[];
+      const parsedResumes = resumes?.map((resume) => JSON.parse(resume.value));
+
+      setResumes(parsedResumes || []);
+      setResumesLoading(false);
+    };
+    loadResumes();
+  }, []);
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
     init();
   }, [auth.isAuthenticated]);
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    <section className="main-section py-16">
-      <div className="page-heading"> 
-        <h1>Track your applications </h1>
-        <h2>Review your submissions and check AI powered feedback.</h2>
-      </div>
-    </section>
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-        {resumes.map(resume => <>
-          <ResumeCard key={resume.id} resume={resume} />
-        </>)}
-      </div>
-      // <div key={resume.id} className="resume-card">
-      //   <img src={resume.imagePath} alt="Resume Image" className="resume-image" />
-      //   <div className="resume-details">
-      //     <h3 className="company-name">{resume.companyName || "Unknown Company"}</h3>
-      //     <p className="job-title">{resume.jobTitle || "Unknown Job Title"}</p>
-      //     <a href={resume.resumePath} download className="download-link">Download Resume</a>
-      //     <div className="feedback-section">
-      //       <h4>Overall Score: {resume.feedback.overallScore}</h4>
-      //       <div className="feedback-categories">
-      //         <div className="category">
-      //           <h5>ATS Score: {resume.feedback.ATS.score}</h5>
-      //           <ul>
-      //             {resume.feedback.ATS.tips.map((tip, index) => (
-      //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
-      //                 {tip.tip}
-      //               </li>
-      //             ))}
-      //           </ul>
-      //         </div>
-      //         <div className="category">
-      //           <h5>Tone & Style Score: {resume.feedback.toneAndStyle.score}</h5>
-      //           <ul>
-      //             {resume.feedback.toneAndStyle.tips.map((tip, index) => (
-      //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
-      //                 {tip.tip} - {tip.explanation}
-      //               </li>
-      //             ))}
-      //           </ul>
-      //         </div>
-      //         <div className="category">
-      //           <h5>Content Score: {resume.feedback.content.score}</h5>
-      //           <ul>
-      //             {resume.feedback.content.tips.map((tip, index) => (
-      //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
-      //                 {tip.tip} - {tip.explanation}
-      //               </li>
-      //             ))}
-      //           </ul>
-      //         </div>
-      //         <div className="category">
-      //           <h5>Structure Score: {resume.feedback.structure.score}</h5>
-      //           <ul>
-      //             {resume.feedback.structure.tips.map((tip, index) => (
-      //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
-      //                 {tip.tip} - {tip.explanation}
-      //               </li>
-      //             ))}
-      //           </ul>
-      //         </div>
-      //         <div className="category">
-      //           <h5>Skills Score: {resume.feedback.skills.score}</h5>
-      //           <ul>
-      //             {resume.feedback.skills.tips.map((tip, index) => (
-      //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
-      //                 {tip.tip} - {tip.explanation}
-      //               </li>
-      //             ))}
-      //           </ul>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
-    )}
-  </main>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section py-16">
+        <div className="page-heading">
+          <h1>Track your applications </h1>
+          <h2>Review your submissions and check AI powered feedback.</h2>
+        </div>
+      </section>
+      {!loadingResumes && resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <>
+              <ResumeCard key={resume.id} resume={resume} />
+            </>
+          ))}
+        </div>
+        // <div key={resume.id} className="resume-card">
+        //   <img src={resume.imagePath} alt="Resume Image" className="resume-image" />
+        //   <div className="resume-details">
+        //     <h3 className="company-name">{resume.companyName || "Unknown Company"}</h3>
+        //     <p className="job-title">{resume.jobTitle || "Unknown Job Title"}</p>
+        //     <a href={resume.resumePath} download className="download-link">Download Resume</a>
+        //     <div className="feedback-section">
+        //       <h4>Overall Score: {resume.feedback.overallScore}</h4>
+        //       <div className="feedback-categories">
+        //         <div className="category">
+        //           <h5>ATS Score: {resume.feedback.ATS.score}</h5>
+        //           <ul>
+        //             {resume.feedback.ATS.tips.map((tip, index) => (
+        //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
+        //                 {tip.tip}
+        //               </li>
+        //             ))}
+        //           </ul>
+        //         </div>
+        //         <div className="category">
+        //           <h5>Tone & Style Score: {resume.feedback.toneAndStyle.score}</h5>
+        //           <ul>
+        //             {resume.feedback.toneAndStyle.tips.map((tip, index) => (
+        //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
+        //                 {tip.tip} - {tip.explanation}
+        //               </li>
+        //             ))}
+        //           </ul>
+        //         </div>
+        //         <div className="category">
+        //           <h5>Content Score: {resume.feedback.content.score}</h5>
+        //           <ul>
+        //             {resume.feedback.content.tips.map((tip, index) => (
+        //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
+        //                 {tip.tip} - {tip.explanation}
+        //               </li>
+        //             ))}
+        //           </ul>
+        //         </div>
+        //         <div className="category">
+        //           <h5>Structure Score: {resume.feedback.structure.score}</h5>
+        //           <ul>
+        //             {resume.feedback.structure.tips.map((tip, index) => (
+        //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
+        //                 {tip.tip} - {tip.explanation}
+        //               </li>
+        //             ))}
+        //           </ul>
+        //         </div>
+        //         <div className="category">
+        //           <h5>Skills Score: {resume.feedback.skills.score}</h5>
+        //           <ul>
+        //             {resume.feedback.skills.tips.map((tip, index) => (
+        //               <li key={index} className={tip.type === "good" ? "tip-good" : "tip-improve"}>
+        //                 {tip.tip} - {tip.explanation}
+        //               </li>
+        //             ))}
+        //           </ul>
+        //         </div>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
+      )}
+    </main>
+  );
 }
